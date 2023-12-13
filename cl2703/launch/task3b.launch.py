e2a4d985200f8bb206244e16474b459a6469f642
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2023-12-13 19:59:01 +0530

    Bringup package `cl2703`
    This just seems to make the most sense. The task scripts seem like
    a misfit under `ur_description`. In addition, this also serves
    as a good place to keep launch files pertaining to specific tasks
    (like `task3b.launch.py` under this new package)
    
    Features introduced by this commit:
    * A new package to house the task scripts
    * A launch file for every task, starting with Task 3B

---
+++ cl2703/launch/task3b.launch.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import os
+import launch
+import launch_ros
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ctx = launch.launch_context.LaunchContext ()
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+                                            description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time'),
+
+        # UR5 Arm and Moveit! stack
+        IncludeLaunchDescription (PythonLaunchDescriptionSource (
+            os.path.join (get_package_share_directory('ur5_moveit'), 'launch/spawn_ur5_launch_moveit.launch.py')
+        )),
+
+        # Navigator2 stack
+        IncludeLaunchDescription (PythonLaunchDescriptionSource (
+            os.path.join (get_package_share_directory('ebot_nav2'), 'launch/ebot_bringup_launch.py')
+        )),
+
+        # Docking service
+        launch_ros.actions.Node(
+            package='ebot_docking',
+            executable='ebot_docking_service_task2b.py',
+        ),
+
+        # Servo node trigger
+        launch.actions.ExecuteProcess(
+            cmd='ros2 service call /servo_node/start_servo std_srvs/srv/Trigger'.split(' '),
+            output='both',
+        ),
+
+        # Aruco detector
+        launch_ros.actions.Node(
+            package='cl2703',
+            executable='task1a.py',
+        ),
+
+        # Warehouse in Gazebo
+        # This MUST be the last entry in the launch. Else the launch crashes
+        # Not sure why. Could be something related to the error handling process of launch
+        IncludeLaunchDescription ( PythonLaunchDescriptionSource (
+            os.path.join (get_package_share_directory('eyantra_warehouse'), 'launch/task3a.launch.py')
+        )),
+
+    ])
